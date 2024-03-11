7a681b27106462f9bce630ee74fa04ad70043a66
Rehan432177 <153973020+Rehan432177@users.noreply.github.com>
2024-03-11 11:45:26 +0500

    Add files via upload

---
+++ javascript Assignment 1/script.js
@@ -0,0 +1,7 @@
+let firstname = prompt("Please enter your first name:");
+let middlename = prompt("Please enter your middle name");
+let lastname = prompt("Please enter your last");
+console.log(firstname +" "+ middlename +" "+lastname);
+alert("Hello, "+ firstname +" "+ middlename + " " + lastname+". Welcome to website!");
+
+
