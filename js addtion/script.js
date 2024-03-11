95d6c4020c98f3fb4bd6c9098682662e9168d2c8
Rehan432177 <153973020+Rehan432177@users.noreply.github.com>
2024-03-11 11:46:07 +0500

    Add files via upload

---
+++ js addtion/script.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter your Frist Number");
+let num2 = prompt("Enter your Second Number");
+let num3 = prompt("Enter your third number")
+
+
+let convertnum1 = parseInt (num1);
+let convertnum2 = parseInt (num2);
+let convertnum3 = parseInt (num3);
+
+console.log("This is addition")
+
+console.log(parseInt(num1) + parseInt(num2) + parseInt(num2));
